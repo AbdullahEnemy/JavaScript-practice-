2d48f307c8647faef61c53cf248f659cfc5a0689
AbdullahEnemy <akbuddy8@gmail.com>
2025-07-04 00:28:26 +0500

    array methods

--- functions.js
+++ functions.js
@@ -46,9 +46,36 @@ console.log(arrowCountVowel("abdullah Fayyaz"));
 //for each (to exectute a function for each element of an array)
 //higher order function are functions that take or return a function so foreach is a higher order fucction
 //Practice Question
-let arr=[1,2,3,4,5];
+let arr=[1,2,3,4,5]
 function display(num){
     console.log(num);
 }
 arr.forEach((val,idx,arr)=>{console.log(val**2,idx,arr)});
 arr.forEach(display);
+//map creates a new array with some operations on the array elements
+let newArr=arr.map(val=>{return val*val})
+console.log(newArr);
+//filter create a new array of elements that give true for a condition
+let evenArr=arr.filter((val=>{
+    return (val%2===0)
+}))
+console.log(evenArr)
+//performs some operations & return a single value
+const output=arr.reduce((res,cur)=>{return res+cur});
+console.log(output);
+//practice question 
+let studentMarks=[50,90,95,92,45,98,96,36,74,92,93];
+let sArr=studentMarks.filter((val)=>{if(val>=90)
+{return val};
+})
+console.log(sArr);
+//let n=prompt("enter a number");
+let n=10;
+let numArr=[];
+for(let i=1;i<=n;i++){
+    numArr.push(i);
+}
+const sumofArr=numArr.reduce((res,cur)=>{return res+cur});
+const productofArr=numArr.reduce((res,cur)=>{return res*cur});
+console.log(sumofArr);
+console.log(productofArr);
